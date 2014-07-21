12c8dbb3329b39e7391a3843a78b1d7ac6c288b4
los.ilia.ru@gmail.com <los.ilia.ru@gmail.com@33b1fee2-c1b5-11de-a759-a54c9d3330c2>
2014-07-21 11:16:32 +0000

    - removed all the code before migration to standard java webapp instead of gae-application.

--- trunk/lib/gdata/installall.bat
+++
@@ -1,5 +0,0 @@
-call install.bat gdata-core 1.0 java/lib/gdata-core-1.0.jar
-call install.bat gdata-client 1.0 java/lib/gdata-client-1.0.jar
-call install.bat gdata-client-meta 1.0 java/lib/gdata-client-meta-1.0.jar
-call install.bat gdata-calendar 2.0 java/lib/gdata-calendar-2.0.jar
-call install.bat gdata-calendar-meta 2.0 java/lib/gdata-calendar-meta-2.0.jar
